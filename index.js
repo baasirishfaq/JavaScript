7440f971d7630809b2a6755a4367a0f7a4359000
Baasir Ishfaq <baasirishfaq@gmail.com>
2025-05-10 18:55:15 -0400

    new

---
+++ index.js
@@ -0,0 +1,12 @@
+// console.log("Hello");
+// console.log("I like pizza"); // print command
+
+// // window.alert("This is an alert"); // for pop up window
+
+
+// document.getElementById("myH1").textContent = "Hello World"; // change the text of the h1 element
+// document.getElementById("myP").textContent = "lorem ipsum"; // change the text of the p element
+
+
+let x;
+x = 100;
