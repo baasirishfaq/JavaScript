e98cea151cc33b64dff4f6b2916e6570a0c09d24
Baasir Ishfaq <baasirishfaq@gmail.com>
2025-05-11 20:18:12 -0400

    username fetching basic

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-// console.log("Hello");
-// console.log("I like pizza"); // print command
+let username
 
-// // window.alert("This is an alert"); // for pop up window
+document.getElementById("mySubmit").onclick = function() {
+
+    username = document.getElementById("myText").value;
+    if (username.length > 0) {
+        document.getElementById("myText").value = "";
+        window.alert("Hello " + username);
+        
+    }
+    console.log(username);
+    document.getElementById("myText").textContent = ""; // clear the username field after submission
+}
 
 
-// document.getElementById("myH1").textContent = "Hello World"; // change the text of the h1 element
-// document.getElementById("myP").textContent = "lorem ipsum"; // change the text of the p element
 
 
-let x;
-x = 100;
