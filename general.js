e052294de8d54b1bde70a4e9f903256efd4a8dc0
Baasir Ishfaq <baasirishfaq@gmail.com>
2025-06-01 23:54:13 -0400

    map method

--- general.js
+++ general.js
@@ -1,10 +1,11 @@
+let chs = ['a b c d e f g h i j k l m n o'];
 
-    let charset = 'abcdefghijklmnopqrstuvwxyz;', password = "";
-    let flag = 1;
 
-    for (let i = 0; i < 5; i++) {
-        password += flag == 1 ? (charset.charAt(Math.floor(Math.random() * charset.length)) + Math.round(Math.random() * 9)) 
-            : charset.charAt(Math.floor(Math.random() * charset.length)).toUpperCase(); flag = 1 - flag;
-    }
+let chs2 = chs.map(uppercase);
 
-    console.log(`password is ${password} and length is ${password.length}`); // Output: Testing
+
+function uppercase(str){
+    return str.toUpperCase();
+}
+
+console.log(...chs2);
